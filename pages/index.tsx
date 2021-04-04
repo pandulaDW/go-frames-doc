500f614f1f1c86781a29f571231620a47113e85d
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-04-04 11:38:49 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>TS-Contentful</title>
+      </Head>
+      <h1>Works</h1>
+    </div>
+  );
+}
